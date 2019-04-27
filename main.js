751004fae247ae83f30bcdf3fda65a83f6646ceb
uabdm <50030989+uabdm@users.noreply.github.com>
2019-04-26 22:01:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Options
-const CLIENT_ID = '221115922051-9hoc622dlq3cnqup9i71g2ski5jkc8rh.apps.googleusercontent.com';
+/*const CLIENT_ID = '221115922051-9hoc622dlq3cnqup9i71g2ski5jkc8rh.apps.googleusercontent.com';
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';*/
 
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
